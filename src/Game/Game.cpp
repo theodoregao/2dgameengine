ef519c6cf787c2a89cb2788103dd30b22ea1e1ae
shungao <theodore.gao.1986@gmail.com>
2022-12-04 15:16:35 -0800

    Load tilemap to AssetStore.

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 
+#include <fstream>
 #include <glm/glm.hpp>
 #include <typeinfo>
 
@@ -55,7 +56,9 @@ void Game::Run() {
   }
 }
 
-void Game::Setup() {
+void Game::Setup() { LoadLevel(1); }
+
+void Game::LoadLevel(int level) {
   Logger::Log(typeid(this).name(), "Setup()");
   registry->AddSystem<MovementSystem>();
   registry->AddSystem<RenderSystem>();
@@ -64,6 +67,31 @@ void Game::Setup() {
                          "./assets/images/tank-panther-right.png");
   assetStore->AddTexture(renderer, "truck-image",
                          "./assets/images/truck-ford-right.png");
+  assetStore->AddTexture(renderer, "tilemap-image",
+                         "./assets/tilemaps/jungle.png");
+  int tileSize = 32;
+  double tileScale = 1.0;
+  int mapNumCols = 25;
+  int mapNumRows = 20;
+  std::fstream mapFile;
+  mapFile.open("./assets/tilemaps/jungle.map");
+  for (int y = 0; y < mapNumRows; y++) {
+    for (int x = 0; x < mapNumCols; x++) {
+      char ch;
+      mapFile.get(ch);
+      int srcRectY = std::atoi(&ch) * tileSize;
+      mapFile.get(ch);
+      int srcRectX = std::atoi(&ch) * tileSize;
+      mapFile.ignore();
+
+      Entity tile = registry->CreateEntity();
+      tile.AddComponent<TransformComponent>(
+          glm::vec2(x * (tileScale * tileSize), y * (tileScale * tileSize)),
+          glm::vec2(tileScale, tileScale), 0.0);
+      tile.AddComponent<SpriteComponent>("tilemap-image", tileSize, tileSize,
+                                         srcRectX, srcRectY);
+    }
+  }
 
   Entity tank = registry->CreateEntity();
   tank.AddComponent<TransformComponent>(glm::vec2(10.0, 30.0),
--- src/Game/Game.h
+++ src/Game/Game.h
@@ -19,6 +19,7 @@ class Game {
   void Initialize();
   void Run();
   void Setup();
+  void LoadLevel(int level);
   void ProcessInput();
   void Update();
   void Render();
